bd79d5d7914c267e80c254b64c5f7e2343cae2c3
Harshvardhan Singh <harshwardhan.s@engineerbabu.in>
2020-10-01 14:32:12 +0530

    Added automated mouse script for trackers

---
+++ harshvardhan/auto_mouse.py
@@ -0,0 +1,33 @@
+import pyautogui
+import time
+
+i=1
+a = 0
+#while for the infinite loop for mouse to run
+while i:
+    #Error handling
+    try:
+        pyautogui.moveTo(200, 100)
+        time.sleep(100)
+        pyautogui.moveTo(100, 600)
+        time.sleep(100)
+        pyautogui.moveTo(600, 900)
+        time.sleep(100)
+
+        a += 1
+        print("Iterations = ",a)
+    #if exception occurs
+    except:
+        print('Exception')
+
+
+# In[22]:
+
+
+pyautogui.size()
+
+
+# In[23]:
+
+
+pyautogui.moveTo(10, 10)
