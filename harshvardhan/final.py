c86100c6eabb62165fb4423efda50226ad1abbd1
Harshvsr1 <35589996+Harshvsr1@users.noreply.github.com>
2020-10-20 13:51:32 +0530

    Create final.py

---
+++ harshvardhan/final.py
@@ -0,0 +1,7 @@
+import math #importing class
+
+num = float(input("Enter any float number: "))
+print("math.floor(num): ", math.floor(num))
+
+num = float(input("Enter another float number: "))
+print("math.floor(num): ", math.floor(num))
